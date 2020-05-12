7e79702d7c8bde59190727502fdac52a30e033ba
oisiiudonn <oisii.udonn75@gmail.com>
2020-05-12 23:59:12 +0900

    不要フォルダの削除

--- c/C_larning/1/1-1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-main(){
-    for(int i=0;i<10;i++){
-    printf("hello, world");
-    }
-}
--- c/C_larning/2/2-1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("2-1!");
-    return 0;
-}
